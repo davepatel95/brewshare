16476a0d4bacb3c6f6531cc649d0046a5d22daac
Dave Patel <davepatel95@gmail.com>
2019-05-25 19:21:54 -0700

    added mock data to inex files

---
+++ public/index.js
@@ -0,0 +1,61 @@
+let MOCK_COFFEE_REVIEWS = {
+    "coffeeReviews": [
+    {
+        "id": "11111",
+        "roasters": "Augie's Coffee Roasters",
+        "beansOrigin": "Ethiopia",
+        "flavorNotes": ["caramel", "cherry", "citrus"],
+        "brewMethod": "Chemex",
+        "description": "Delicious coffee with just the right touch of caramel sweetness. The fruitier notes were tougher to identify but overall a great brew. Very smooth and rich."
+    },
+    {
+        "id": "22222",
+        "roasters": "Temple Coffee Roasters",
+        "beansOriginin": "Colombia",
+        "flavorNotes": ["nutty", "lavender", "citrus"],
+        "brewMethod": "Kalita",
+        "description": "Here is some filler. I love coffee. It keeps me productive. It wakes me up. It pushes me to new limits. It is a nectar I cherish."
+    },
+    {
+        "id": "33333",
+        "roasters": "Augie's Coffee Roasters",
+        "beansOriginin": "Guatemala",
+        "flavorNotes": ["grapefruit", "zucchini", "citrus"],
+        "brewMethod": "French Press",
+        "description": "Here is some filler. I love coffee. It keeps me productive. It wakes me up. It pushes me to new limits. It is a nectar I cherish."
+    },
+    {
+        "id": "44444",
+        "roasters": "Augie's Coffee Roasters",
+        "beansOriginin": "Brazil",
+        "flavorNotes": ["chocolate", "caramel", "nutty"],
+        "brewMethod": "V60",
+        "description": "Here is some filler. I love coffee. It keeps me productive. It wakes me up. It pushes me to new limits. It is a nectar I cherish."
+    },
+]
+}
+
+function getCoffeeReviews(callback) {
+setTimeout(function() {callback(MOCK_COFFEE_REVIEWS)}, 1);
+}
+
+function displayCoffeeReviews(data) {
+for (index in data.coffeeReviews) {
+    $('body').append(
+        '<p>Roasters: ' + data.coffeeReviews[index].roasters + '</p>' +
+        '<p>Beans Origin: ' + data.coffeeReviews[index].beansOrigin + '</p>' +
+        '<p>Flavor Notes: ' + data.coffeeReviews[index].flavorNotes + '</p>' +
+        '<p>Brew Method: ' + data.coffeeReviews[index].brewMethod + '</p>' +
+        '<p>Description: ' + data.coffeeReviews[index].description + '</p>');
+        console.log(data.coffeeReviews);
+}
+
+}
+
+function getAndDisplayCoffeeReviews() {
+getCoffeeReviews(displayCoffeeReviews);
+}
+
+$(function() {
+getAndDisplayCoffeeReviews();
+})
