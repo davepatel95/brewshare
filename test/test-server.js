e88ada3bc5c4da080c5ca9ca106464b728d02c54
Dave Patel <davepatel95@gmail.com>
2019-06-04 16:09:54 -0700

    added tests

--- test/test-server.js
+++ test/test-server.js
@@ -8,10 +8,54 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('index page', function() {
-    it("should contain Hello World", function() {
+    it("should exist", function() {
         return chai
             .request(app)
-            .get("/")
+            .get("/index.html)
+            .then(function(res) {
+                expect(res).to.have.status(200);
+            });
+    });
+});
+
+describe('create page', function() {
+    it("should exist", function() {
+        return chai
+            .request(app)
+            .get("/create.html")
+            .then(function(res) {
+                expect(res).to.have.status(200);
+            });
+    });
+});
+
+describe('css page', function() {
+    it("should exist", function() {
+        return chai
+            .request(app)
+            .get("/index.css")
+            .then(function(res) {
+                expect(res).to.have.status(200);
+            });
+    });
+});
+
+describe('index js page', function() {
+    it("should exist", function() {
+        return chai
+            .request(app)
+            .get("/index.js")
+            .then(function(res) {
+                expect(res).to.have.status(200);
+            });
+    });
+});
+
+describe('review page', function() {
+    it("should exist", function() {
+        return chai
+            .request(app)
+            .get("/review.html")
             .then(function(res) {
                 expect(res).to.have.status(200);
             });
