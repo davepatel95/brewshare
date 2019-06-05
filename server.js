c0c4210b129345b12010d64d775fe9d2be3bad2d
Dave Patel <davepatel95@gmail.com>
2019-06-05 11:23:31 -0700

    added config files, router and models directories

--- server.js
+++ server.js
@@ -3,6 +3,13 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+
+app.get('/create', (req, res) => {
+    res.json()
+})
+
+
+
 if (require.main === module) {
     app.listen(process.env.PORT || 8080, function() {
         console.info(`App is listening on ${this.address().port}`);
