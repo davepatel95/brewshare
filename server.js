591c3b02668ee29b288e5170c7664971af166c8c
Dave Patel <davepatel95@gmail.com>
2019-06-05 15:43:16 -0700

    reset server file

--- server.js
+++ server.js
@@ -1,51 +1,12 @@
 'use strict';
 
 const express = require('express');
-const mongoose = require('mongoose');
-
-const {DATATBASE_URL, PORT} = require('./config');
-
 const app = express();
-
-
 app.use(express.static('public'));
-
-let server;
-
-function runServer(databaseUrl = DATATBASE_URL, port = PORT) {
-    return new Promise((resolve, reject) => {
-        mongoose.connect(databaseUrl, err => {
-            if (err) {
-                return reject(err);
-            }
-            server = app.listen(port, ()=> {
-                console.log('Your app is listening on port ${port}');
-                resolve();
-            })
-            .on('error', err => {
-                mongoose.disconnect();
-                reject(err);
-            });
-        });
-    });
-}
-
-function closeServer() {
-    return mongoose.disconnect().then(() => {
-        return new Promise((resolve, reject) => {
-            console.log('Closing server');
-            server.close(err => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve();
-            });
-        });
-    });
-}
-
 if (require.main === module) {
-    runServer().catch(err => console.error(err));
+    app.listen(process.env.PORT || 8080, function() {
+        console.info(`App is listening on ${this.address().port}`);
+    });
 }
 
-module.exports = {runServer, app, closeServer};
+module.exports = app;
