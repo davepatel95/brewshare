0e04d357cea23ddad0dc7fe6707b02ea8e38288f
Dave Patel <davepatel95@gmail.com>
2019-06-10 17:53:57 -0700

    added server code, database

--- server.js
+++ server.js
@@ -1,12 +1,57 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require("./config");
+
+
 const app = express();
+app.use(express.json());
 app.use(express.static('public'));
-if (require.main === module) {
-    app.listen(process.env.PORT || 8080, function() {
-        console.info(`App is listening on ${this.address().port}`);
+
+let server;
+function runServer(databaseUrl, port = PORT,) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(
+            databaseUrl, { useNewUrlParser: true}, 
+            err => {
+                if (err) {
+                    return reject(err);
+                }
+                server = app
+                    .listen(port, () => {
+                        console.log(`Your app is listening on port ${port}`);
+                        resolve();
+                    })
+                    .on("error", err => {
+                        mongoose.disconnect();
+                        reject(err);
+                    });
+            }
+        );
     });
 }
 
-module.exports = app;
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log("Closing server");
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+
+module.exports = {app, runServer, closeServer};
