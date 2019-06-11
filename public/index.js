2bc87aee39718ac4c036b8390fb8ab594fd0a753
Dave Patel <davepatel95@gmail.com>
2019-06-10 17:52:59 -0700

    fixed modal windows for reviews

--- public/index.js
+++ public/index.js
@@ -43,24 +43,50 @@ function getCoffeeReviews(callback) {
 setTimeout(function() {callback(MOCK_COFFEE_REVIEWS)}, 1);
 }
 
-function displayCoffeeReviews(data) {
-for (index in data.coffeeReviews) {
-    $('body').append(
-        '<div class="review-div">' +
+ function displayCoffeeReviews(data) {
+ for (index in data.coffeeReviews) {
+     $('body').append(
+    //     '<div class="container">' +  
+    //     '<button class="show-modal open-modal">Open Modal</button>' +
+        
+    //     '<div class="modal">' +
+    //       '<a class="close-modal" href="#">X</a>' +
+    //       '<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt sit quas aut labore nam quis qui eos quasi soluta sunt nihil voluptate expedita. Aliquam aliquid excepturi ex ab fuga ipsa?</div>' + 
+    //     '</div>' +
+        
+    //   '</div>');
+    //  }
+        '<div class="container">' +
+        '<a href="#" class="show-modal"><p class="open-modal">Title: ' + data.coffeeReviews[index].title + '</p></a></div>' + 
+        '<div class="modal">' +
+        '<a class="close-modal" href="#">X</a>' +
         '<p>Roasters: ' + data.coffeeReviews[index].roasters + '</p>' +
         '<p>Beans Origin: ' + data.coffeeReviews[index].beansOrigin + '</p>' +
         '<p>Flavor Notes: ' + data.coffeeReviews[index].flavorNotes + '</p>' +
         '<p>Brew Method: ' + data.coffeeReviews[index].brewMethod + '</p>' +
         '<p>Description: ' + data.coffeeReviews[index].description + '</p></div>');
-        console.log(data.coffeeReviews);
+        console.log(data.coffeeReviews[index].title);
     }
+        renderModal();
 }
 
 function getAndDisplayCoffeeReviews() {
 getCoffeeReviews(displayCoffeeReviews);
 }
 
+function renderModal() {
+    var modal = $('.modal');
+    $('.show-modal').click(function() {
+    modal.fadeIn();
+    });
+
+    $('.close-modal').click(function() {
+    modal.fadeOut();
+    });
+}
+
+
+
 $(function() {
 getAndDisplayCoffeeReviews();
-renderCreatePage();
 })
