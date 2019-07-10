22ccea73272456a055a1ac1ab5bce65f0745892b
Dave Patel <davepatel95@gmail.com>
2019-07-10 15:56:20 -0700

    added dotenv import

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ exports.DATABASE_URL =  'mongodb+srv://evadletap:DLjDyQQ2QJp1SDUv@cluster0-xiteu
 exports.TEST_DATABASE_URL = 'mongodb+srv://evadletap:DLjDyQQ2QJp1SDUv@cluster0-xiteu.mongodb.net/test-brewshare?retryWrites=true&w=majority';
 exports.PORT = process.env.PORT || 8080;
 
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'uhyxv7itha';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
