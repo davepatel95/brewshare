b670b116d37af1ec07cad2c1a726a8f860145e82
Dave Patel <davepatel95@gmail.com>
2019-07-11 11:49:13 -0700

    test 2

--- auth/strategies.js
+++ auth/strategies.js
@@ -38,7 +38,7 @@ const localStrategy = new LocalStrategy((username, password, callback) => {
         });
 });
 
-console.log(JWT_SECRET);
+console.log(process.env.JWT_SECRET);
 const jwtStrategy = new JwtStrategy(
     {
         secretOrKey: process.env.JWT_SECRET,
