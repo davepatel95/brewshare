2b220e05f5b1aae3cb4e36c7962885e7e01f362c
Dave Patel <davepatel95@gmail.com>
2019-07-15 11:44:23 -0700

    adjusted process.env code for dbs, secrets

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 'use strict';
-exports.DATABASE_URL =  process.env.DATABASE_URL; //|| 'mongodb+srv://evadletap:DLjDyQQ2QJp1SDUv@cluster0-xiteu.mongodb.net/BrewShare?retryWrites=true&w=majority';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb+srv://evadletap:DLjDyQQ2QJp1SDUv@cluster0-xiteu.mongodb.net/test-brewshare?retryWrites=true&w=majority';
+exports.DATABASE_URL =  process.env.DATABASE_URL || 'mongodb://localhost/brewshare';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-brewshare';
 exports.PORT = process.env.PORT || 8080;
-
-exports.JWT_SECRET = process.env.JWT_SECRET || 'htethrsdrteg';
+exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
