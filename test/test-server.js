d5094587f76a69fffafa2a957d5a7af25e6b31d8
Dave Patel <davepatel95@gmail.com>
2019-07-17 11:46:23 -0700

    checking dotenv error again

--- test/test-server.js
+++ test/test-server.js
@@ -3,21 +3,52 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const {app, runServer, closeServer } = require("../server");
+const { TEST_DATABASE_URL } = require('../config');
+const { seedDatabase, tearDownDb } = require('./seedDatabase');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe('index page', function() {
-    it("should send html for landing page", function() {
-        return chai
-            .request(app)
-            .get("/index.html")
-            .then(function(res) {
-                expect(res).to.have.status(200);
-                expect(res).to.be.html;
-            });
-    });
-});
+
+// describe('Server', function() {
+//     before(function() {
+//         return runServer(TEST_DATABASE_URL);
+//     });
+
+//     beforeEach(function() {
+//         return seedDatabase();
+//     });
+
+//     afterEach(function() {
+//         return tearDownDb();
+//     });
+
+//     after(function() {
+//         return closeServer();
+//     });
+
+//     describe('index page', function() {
+//         it("should send html for landing page", function() {
+//             return chai
+//                 .request(app)
+//                 .get("/index.html")
+//                 .then(function(res) {
+//                     expect(res).to.have.status(200);
+//                     expect(res).to.be.html;
+//                 });
+//         });
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
 
 describe('create page', function() {
     it("should send html for create brew page", function() {
