cb2804361e8873176b425ccade7091d6f59b56c7
Dave Patel <davepatel95@gmail.com>
2019-08-05 15:34:07 -0700

    updated nodemon dependency

--- brews/router.js
+++ brews/router.js
@@ -13,6 +13,7 @@ const {Brew} = require('./models');
 passport.use(jwtStrategy);
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
+// GET requests to /brews
 router.get('/', (req, res) => {
      Brew
         .find()
@@ -28,12 +29,19 @@ router.get('/', (req, res) => {
         });
 });
 
-// GET requests to /brews
-
+router.get('/:id', (req, res) => {
+    Brew
+        .findById(req.params.id)
+        .then(brew => res.json(brew))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({message: 'Internal Server Error'});
+        });
+});
 
 // POST requests to /brews
 router.post('/', (req, res) => {
-    const requiredFields = ['title', 'roasters', 'beansOrigin', 'flavorNotes', 'brewMethod', 'description'];
+    const requiredFields = ['title', 'author', 'roasters', 'beansOrigin', 'flavorNotes', 'brewMethod', 'description'];
     for (let i = 0; i < requiredFields.length; i++) {
       const field = requiredFields[i];
       if (!(field in req.body)) {
@@ -46,6 +54,7 @@ router.post('/', (req, res) => {
     Brew
         .create({
             title: req.body.title,
+            author: req.body.author,
             roasters: req.body.roasters,
             beansOrigin: req.body.beansOrigin,
             flavorNotes: req.body.flavorNotes,
