08e76c7558841038c56f99998dddcb6bf0cf3000
Dave Patel <davepatel95@gmail.com>
2019-08-07 16:10:14 -0700

    removed temp string key

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 exports.DATABASE_URL =  process.env.DATABASE_URL || 'mongodb://localhost/brewshare';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-brewshare';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET || 'dgssaegaegazvds';
+exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '24h';
