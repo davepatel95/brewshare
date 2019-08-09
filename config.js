57897be27991d4669e82d8a359b2106851775415
Dave Patel <davepatel95@gmail.com>
2019-08-09 11:10:44 -0700

    removed local database to test

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL =  process.env.DATABASE_URL || 'mongodb://localhost/brewshare';
+exports.DATABASE_URL =  process.env.DATABASE_URL;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'uoigoiug';
